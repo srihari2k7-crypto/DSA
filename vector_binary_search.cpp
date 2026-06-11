d37688f7b29276e768796665779ba10bea4f074e
srihari2k7-crypto <srihari2k7@gmail.com>
2026-06-11 09:24:04 +0530

    Problem 12: Vectors-Binary search

---
+++ vector_binary_search.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution
+{
+	public:
+		int binarySearch(vector<int>& vec, int target)
+		{
+			int start = 0;
+			int end = vec.size()-1;
+			while(start<=end)
+			{
+				int mid = start + (end-start)/2;
+				if(vec[mid]==target)
+				{
+					return mid;
+				}
+				else if(vec[mid]>target)
+				{
+					end = mid-1;
+				}
+				else if(vec[mid]<target)
+				{
+					start = mid+1;
+				}
+			}
+			return -1;
+			
+		}
+};
+int main()
+{
+	vector<int> samp = {0,1,5,7,9,12};
+	for(int val : samp)
+	{
+		cout<<val<<" ";
+	}
+	cout<<endl;
+	int tar =2;
+	Solution s1;
+	cout<<s1.binarySearch(samp, tar)<<endl;
+}
